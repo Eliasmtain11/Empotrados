eda1ff0cf33021152fb4becae0c8785c433c3fd6
Elias Munoz <e.munozt.2023@alumnos.urjc.es>
2025-10-23 10:55:21 +0200

    acabada

--- src/cyclictestURJC.c
+++ src/cyclictestURJC.c
@@ -16,10 +16,6 @@
 #define NUMERO_DE_CILCOS 60000
 #define PRIORITY 99
 
-void *thread_job(void *arg);
-long long long_abs(long long n);
-long long time_diff_ns(struct timespec start, struct timespec end);
-
 typedef struct{
     int id;
     int cpu;
@@ -29,16 +25,29 @@ typedef struct{
     long long latencia_media;
     long long latencia_max;
     long long *array_de_latencias;
+    int length;
 } lat_info;
 
+
+void *thread_job(void *arg);
+long long long_abs(long long n);
+long long time_diff_ns(struct timespec start, struct timespec end);
+int array_to_csv(int file, long long *array, int length, int cpu);
+void salida_controlada(pthread_t *hilos, thread_info *hilos_cpu, lat_info *latencias, int latency_target_fd, int file);
+
+
 int main(int argc, char* argv[]) {
-    int i, err,latency_target_fd;
+    int i, err,latency_target_fd, file;
     lat_info *latencia_cpu;
     int N = (int) sysconf(_SC_NPROCESSORS_ONLN); // Calculamos el numero de cores que hay
     long long latencia_media_total, latencia_max_total;
     struct timespec start, now;
     pthread_t *hilos = malloc(N * sizeof(pthread_t));
     thread_info *hilos_cpu = malloc(N * sizeof(thread_info));
+    lat_info *latencias = malloc(N * sizeof(lat_info));
+    char *filename = "datos_latencia.csv";
+    
+    
 
     latencia_media_total = 0;
     latencia_max_total = 0;
@@ -61,29 +70,42 @@ int main(int argc, char* argv[]) {
         for(i = 0; i != N; i++) {
             pthread_join(hilos[i], (void**)&latencia_cpu);
             if(latencia_cpu != NULL){
-                latencia_media_total = latencia_cpu->latencia_media;
-                if(latencia_cpu->latencia_media > latencia_max_total) {
-                    latencia_max_total = latencia_cpu->latencia_media;
-                }
-                printf("[%d]    latencia media = %lld ns.  |  max = %lld ns\n",i,latencia_cpu->latencia_media, latencia_cpu->latencia_max);
+                latencias[i] = *latencia_cpu;
                 free(latencia_cpu);
             }
             else{
+                free(latencias);
                 free(hilos);
                 free(hilos_cpu);
                 free(latencia_cpu);
+                close(latency_target_fd);
+                exit(EXIT_FAILURE);
+            }    
+        }
+        file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
+        char *header = "CPU, NUMERO_ITERACION, LATENCIA";
+        if (write(file, header, strlen(header)) < 0) {
+                fprintf(stderr,"Error al escribir en el archivo");
+                salida_controlada(hilos,hilos_cpu, latencias, latency_target_fd, file);
                 exit(EXIT_FAILURE);
             }
-            
+        for(i = 0; i != N; i++) {
+            latencia_media_total = latencias[i].latencia_media;
+                if(latencias[i].latencia_media > latencia_max_total) {
+                    latencia_max_total = latencias[i].latencia_media;
+                }
+                err = array_to_csv(file,latencias[i].array_de_latencias, latencias[i].length, i);
+                if(err != 0) {
+                    salida_controlada(hilos,hilos_cpu, latencias, latency_target_fd, file);
+                    exit(EXIT_FAILURE);
+                }
+                printf("[%d]    latencia media = %lld ns.  |  max = %lld ns\n",i,latencias[i].latencia_media, latencias[i].latencia_max);
         }
-        printf("Total latencia media = %lld ns.  |  max = %lld ns\n", latencia_media_total, latencia_max_total);
-        
-    free(hilos);
-    free(hilos_cpu);
-    close(latency_target_fd);
+        printf("Total latencia media = %lld ns.  |  max = %lld ns\n", latencia_media_total, latencia_max_total);   
+
+    salida_controlada(hilos,hilos_cpu, latencias, latency_target_fd, file);
     exit(EXIT_SUCCESS);
 }   
-
 
 void *thread_job(void *arg) {
     struct timespec start_lat, end_lat, start, now;
@@ -132,14 +154,55 @@ void *thread_job(void *arg) {
         latencia_cpu->latencia_media = latencia_media / n;
         latencia_cpu->latencia_max = latencia_max;
         latencia_cpu->array_de_latencias = array_de_latencias;
+        latencia_cpu->length = n;
     }
     else {
         latencia_cpu = NULL;
     }
-   
     return(latencia_cpu);
 }
 
 long long time_diff_ns(struct timespec start, struct timespec end) {
     return((end.tv_sec - start.tv_sec) * SEC_TO_NANOSEC + (end.tv_nsec - start.tv_nsec));
+}
+
+int array_to_csv(int file, long long *array, int length,int cpu) {
+    int i,len;
+    char buffer[128];
+    
+    if(array != NULL) {
+
+        if (file < 0) {
+            fprintf(stderr,"Error al abrir o crear el archivo");
+            return(-1);
+        }
+        // Escribimos la cabecera
+        
+        for (i = 0; i<length; i++) {
+            if (i < length - 1) {
+                len = snprintf(buffer, sizeof(buffer), "%d,%d,%lld,\n", cpu,i,array[i]);
+            }
+            else {
+                len = snprintf(buffer, sizeof(buffer), "%d,%d,%lld\n", cpu,i,array[i]);
+            }
+            if (write(file, buffer, len) < 0) {
+                fprintf(stderr,"Error al escribir en el archivo");
+                return(-1);
+            }
+        }
+        return(0);
+    }
+    else {
+        return(-1);
+    }
+
+
+}
+void salida_controlada(pthread_t *hilos, thread_info *hilos_cpu, lat_info *latencias, int latency_target_fd, int file) {
+    free(latencias);
+    free(hilos);
+    free(hilos_cpu);
+    close(latency_target_fd);
+    close(file);
+
 }
